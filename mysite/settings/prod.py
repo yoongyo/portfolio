1848dabed840c9cecce354a47d8bea76e5e8d5b1
yoongyo <jyg0172@naver.com>
2021-06-30 14:35:03 +0900

    Ad

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -10,8 +10,8 @@ db_from_env = dj_database_url.config(env='DATABASE_URL', conn_max_age=500)
 # 기존 DATABASES
 DATABASES['default'].update(db_from_env)
 
-with open(os.path.join(BASE_DIR, 'secret.json'), 'r') as f:
-    secret = json.loads(f.read())
+# with open(os.path.join(BASE_DIR, 'secret.json'), 'r') as f:
+#     secret = json.loads(f.read())
 
 
 MEDIA_URL = '/media/'
